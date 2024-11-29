803748cd4173f2812e718845bc7ef8f5a08fc31c
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-11-29 19:39:29 +0530

    admin order page

--- app/admin-dashboard/layout.tsx
+++ app/admin-dashboard/layout.tsx
@@ -1,23 +1,19 @@
-
 "use client";
 
 import React from "react";
 import { AppSidebar } from "@/components/admin/AppSidebar";
 import ProtectedRoute from "@/providers/ProtectedRoute";
 
-
-
-export default function AdminLayout({ children } : Readonly<{
-    children: React.ReactNode;
-  }>) {
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <div className="flex h-screen ">
       {/* <ProtectedRoute> */}
-      <AppSidebar>
-        {children}
-      </AppSidebar>
+      <AppSidebar>{children}</AppSidebar>
       {/* </ProtectedRoute> */}
-   
     </div>
   );
 }
--- app/admin-dashboard/orders/page.tsx
+++ app/admin-dashboard/orders/page.tsx
@@ -1,9 +1,10 @@
+import ViewOrders from '@/components/admin/viewOrders/main'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <h1>I am orders page</h1>
+      <ViewOrders/>
     </div>
   )
 }
